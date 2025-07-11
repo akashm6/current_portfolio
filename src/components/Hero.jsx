5515ac69e1b99e5705e843d8af288b6a0a823f15
Akash Mohan <akhikm4@gmail.com>
2025-07-11 11:44:06 -0500

    drive resume view instead

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -68,7 +68,7 @@ export default function Hero() {
             <Linkedin className="w-5 h-5" />
           </a>
           <a
-            href="/AkashMohanResume2025.pdf"
+            href="`https://docs.google.com/gview?embedded=true&url=https://akashmohan.me/AkashMohanResume2025.pdf`"
             target="_blank"
             className="hover:text-purple-500 transition-colors"
           >

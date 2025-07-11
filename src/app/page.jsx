12f90a5d0a60307fcc123246a4dbe0d99008dbb1
Akash Mohan <akhikm4@gmail.com>
2025-07-10 23:01:27 -0500

    full layout added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import Experience from "@/components/Experience";
 import ScrollProgress from "@/components/ScrollProgress";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
     <>
       <ScrollProgress />
       <Navbar />
-      <main className="flex flex-col items-center justify-center px-6 pt-24 gap-16">
+      <main className="flex flex-col items-center justify-center px-6 pt-20 gap-2">
         <Hero />
         <About />
+        <Experience />
+        <Projects />
+        <Skills />
+        <Contact />
       </main>
     </>
   );

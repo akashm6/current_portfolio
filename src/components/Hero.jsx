3fb406514904d55000ca389eaebd9b56e3680b40
Akash Mohan <akhikm4@gmail.com>
2025-08-27 21:51:09 -0500

    updated resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -68,7 +68,7 @@ export default function Hero() {
             <Linkedin className="w-5 h-5" />
           </a>
           <a
-            href="/AkashMohanResume2025_08_18.pdf"
+            href="/AkashMohanResume2025_08_27.pdf"
             target="_blank"
             className="hover:text-purple-500 transition-colors"
           >
